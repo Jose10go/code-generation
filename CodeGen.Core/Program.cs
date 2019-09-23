8b55da80863930e76ded7b6f641bf8b9acc7c3d0
Jose10go <Jose10go2017@gmail.com>
2019-09-23 04:28:07 -0400

    Deleting Serveless Projects and creating working test

--- CodeGen.Core/Program.cs
+++ CodeGen.Core/Program.cs
@@ -21,118 +21,59 @@ namespace Tests
             Console.ReadLine();
 
         }
-
         private async static Task Run(string[] args)
+            {
+                // In order to MSBuildWorkspace find MSBuild Tools
+                //Environment.SetEnvironmentVariable("VSINSTALLDIR", @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise");
+                //Environment.SetEnvironmentVariable("VisualStudioVersion", @"15.0");
+
+                var workspace = MSBuildWorkspace.Create();
+                var project = await workspace.OpenProjectAsync(@"..\..\CodeGen.Core.csproj");
+
+                var compilation = await project.GetCompilationAsync();
+
+                Console.WriteLine($"Diagnostics: {compilation.GetDiagnostics().Count()} \n" +
+                    $"{compilation.GetDiagnostics().Select(diag => diag.GetMessage()).Aggregate((a, b) => $"{a}\n{b}")}");
+
+                Console.WriteLine($"Syntax trees: {compilation.SyntaxTrees.Count()}");
+
+                var ctx = new MyContext(project);
+                //var compilation = await project.GetCompilationAsync();
+
+                //var workspace = MSBuildWorkspace.Create();
+                //var solution = await workspace.OpenSolutionAsync(@"E:\Tony\UH\Maestria\Tesis\project\CSharpCodeGeneration\CSharpCodeGeneration.sln");
+
+                //Project project = solution.Projects.First(proj => proj.Name == "Tests");
+
+                //var codegen = new CodeGenerator(new SolutionEditor(solution));
+
+
+                //var handler = codegen.DIContainer.Resolve<ICommandHandler<CloneCommand<MethodDeclarationSyntax>, CSharpTarget<MethodDeclarationSyntax>,
+                //    MethodDeclarationSyntax, DocumentEditor>>();
+
+                var modifiedDocument = ctx.result.GetChangedDocument();
+
+                var text = await modifiedDocument.GetTextAsync();
+
+                Console.WriteLine(text);
+
+            }
+        public class MyContext:CodeGen.Context.CSharp.ICSharpContext<DocumentEditor>
         {
-            // In order to MSBuildWorkspace find MSBuild Tools
-            //Environment.SetEnvironmentVariable("VSINSTALLDIR", @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise");
-            //Environment.SetEnvironmentVariable("VisualStudioVersion", @"15.0");
+            public DocumentEditor result;
+            public MyContext(Project project)
+            {
+                Document document = project.Documents.First(doc => doc.Name == "in.cs");
 
-            var workspace = MSBuildWorkspace.Create();
-            var project = await workspace.OpenProjectAsync(@"E:\Tony\UH\Maestria\Tesis\project\CSharpCodeGeneration\CodeGen.Core\CodeGen.Core.csproj");
+                var editor = DocumentEditor.CreateAsync(document).Result;
+                var target = new CSharpTarget<MethodDeclarationSyntax>((method)=>true);
+                var cloneCommand = new MethodCloneCommandBuilder()
+                    .WithNewName((method) => method.Identifier.Text + "_generated")
+                    .Build(); 
+                var handler=new MethodCloneCommandHandler() { Target = target, Command = cloneCommand };
 
-            var compilation = await project.GetCompilationAsync();
-
-            Console.WriteLine($"Diagnostics: {compilation.GetDiagnostics().Count()} \n" +
-                $"{compilation.GetDiagnostics().Select(diag => diag.GetMessage()).Aggregate((a, b) => $"{a}\n{b}")}");
-
-            Console.WriteLine($"Syntax trees: {compilation.SyntaxTrees.Count()}");
-
-            //var compilation = await project.GetCompilationAsync();
-
-            //var workspace = MSBuildWorkspace.Create();
-            //var solution = await workspace.OpenSolutionAsync(@"E:\Tony\UH\Maestria\Tesis\project\CSharpCodeGeneration\CSharpCodeGeneration.sln");
-
-            //Project project = solution.Projects.First(proj => proj.Name == "Tests");
-
-            //var codegen = new CodeGenerator(new SolutionEditor(solution));
-
-
-
-            //var handler = codegen.DIContainer.Resolve<ICommandHandler<CloneCommand<MethodDeclarationSyntax>, CSharpTarget<MethodDeclarationSyntax>, 
-            //    MethodDeclarationSyntax, DocumentEditor>>();
-
-            //Document document = project.Documents.First(doc => doc.Name == "in.cs");
-
-            //var editor = await DocumentEditor.CreateAsync(document);
-
-            //var target = new CSharpTarget<MethodDeclarationSyntax>();
-            //var cloneCommand = new MethodCloneCommandHandler() { Target = target, NamingTransformFunction = (method) => method.Identifier.Text + "_generated" };
-
-            //editor = cloneCommand.ProcessDocument(editor);
-
-            //var modifiedDocument = editor.GetChangedDocument();
-
-            //var text = await modifiedDocument.GetTextAsync();
-
-            //Console.WriteLine(text);
-
-            //var compilation = await project.GetCompilationAsync();
-
-            //foreach (var @class in compilation.GlobalNamespace.GetNamespaceMembers().SelectMany(x => x.GetMembers()))
-            //{
-            //    Console.WriteLine(@class.Name);
-            //    Console.WriteLine(@class.ContainingNamespace.Name);
-            //}
-
-            //var classVisitor = new ClassVirtualizationVisitor();
-
-            //foreach (var syntaxTree in compilation.SyntaxTrees)
-            //{
-            //    classVisitor.Visit(syntaxTree.GetRoot());
-            //}
-
-            //var classes = classVisitor.Classes;
+                result= handler.ProcessDocument(editor);
+            }
         }
-
-        //class ClassVirtualizationVisitor : CSharpSyntaxRewriter
-        //{
-        //    public ClassVirtualizationVisitor()
-        //    {
-        //        Classes = new List<ClassDeclarationSyntax>();
-        //    }
-
-        //    public List<ClassDeclarationSyntax> Classes { get; set; }
-
-        //    public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
-        //    {
-        //        node = (ClassDeclarationSyntax)base.VisitClassDeclaration(node);
-        //        Classes.Add(node); // save your visited classes
-        //        return node;
-        //    }
-        //}
     }
-
-    //interface ICommand
-    //{
-    //    IEnumerable<Func<object, bool>> filters { get; }
-    //    void AddFilter(Func<object, bool> filter);
-    //}
-
-    //interface ICommand<in T> : ICommand
-    //{
-    //    new IEnumerable<Func<T, bool>> filters { get; }
-
-    //}
-
-    //class Command : ICommand<string>
-    //{
-    //    internal List<Func<object, bool>> filtersContainer;
-    //    public IEnumerable<Func<string, bool>> filters => ((ICommand)this).filters;
-
-    //    IEnumerable<Func<object, bool>> ICommand.filters => filtersContainer;
-
-    //    public void AddFilter(Func<object, bool> filter)
-    //    {
-    //        filtersContainer.Add(filter);
-    //    }
-    //}
-
-    //static class ICommandExtensors
-    //{
-    //    public static void AddFilter<T>(this ICommand<T> command, Func<T, bool> filter)
-    //    {
-    //        command.AddFilter((object o) => filter((T)o));
-    //    }
-    //}
 }
---
+++ CodeGen.Core/in.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGen
+{
+    public class A
+    {
+        public void hello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
