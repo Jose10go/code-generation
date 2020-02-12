c3e9cef2936c749eac72fed4c5a07c206f08d521
Jose10go <Jose10go2017@gmail.com>
2020-02-12 01:17:05 -0500

    MSBuild Rules

---
+++ CodeGeneration.CSharp/Generator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using System.IO;
+using System.Linq;
+namespace CodeGeneration.CSharp
+{
+    public class Generator : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            var file =Path.Combine("obj", "A.cs");
+            Compiles += ";" + file;
+            File.WriteAllText(file,"jerigonza");
+            return true;
+        }
+
+        [Output]
+        public string Compiles { get; set; }
+    }
+}
