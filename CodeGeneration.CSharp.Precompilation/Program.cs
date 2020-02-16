dce35fb92014020e1b004799b7166126caba3cfa
Jose10go <Jose10go2017@gmail.com>
2020-02-16 03:50:42 -0500

    moving msbuild stuffs to CodeGeneration.Precompilation

---
+++ CodeGeneration.CSharp.Precompilation/Program.cs
@@ -0,0 +1,63 @@
+﻿using CodeGen.CSharp.Context.DocumentEdit;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using static CodeGen.CSharp.Context.DocumentEdit.CSharpContextDocumentEditor;
+
+namespace CodeGeneration.CSharp.Precompilation
+{
+    class Program
+    {
+        static void Main(string project,string[] compiles,string[] transformers)
+        {
+            var instance = MSBuildLocator.RegisterDefaults();
+
+            var workspace = MSBuildWorkspace.Create();
+            workspace.WorkspaceFailed += (sender, args) =>
+                                            workspace.Diagnostics.Add(args.Diagnostic);
+
+            var Project = workspace.OpenProjectAsync(project).Result;
+            if (Project == null)
+                throw new Exception($"Project is null. ({project})");
+
+            var resolver = new CSharpContextDocumentEditor.CSharpAutofacResolver();
+            var Engine = new DocumentEditingCodeGenerationEngine(Project);
+
+            foreach (var item in transformers)
+            {
+                var assembly=LoadAssembly(item);
+                var transformer = LoadTransformer(assembly,Engine,compiles);
+                transformer.Transform();
+            }
+        }
+
+        private static CodeGenerationTransformer LoadTransformer(Assembly assembly,DocumentEditingCodeGenerationEngine Engine,string[] compiles)
+        {
+            var type=assembly.GetTypes().First(x => x.IsSubclassOf(typeof(CodeGenerationTransformer)));//TODO: Why only one transformer by assembly
+            if (type is null) 
+                throw new Exception($"There is no transformer on assembly {assembly.FullName}.");
+            var transformer = Activator.CreateInstance(type);
+            if (transformer is null)
+                throw new Exception($"Cant get instance of {type.Name}.");
+
+            CodeGenerationTransformer trans = transformer as CodeGenerationTransformer;
+            trans.Compiles = compiles;            
+            trans.Engine = Engine;
+            return transformer as CodeGenerationTransformer;
+        }
+
+        private static Assembly LoadAssembly(string path)
+        {
+            var asembly = Assembly.LoadFrom(path);
+            if (asembly is null)
+                throw new Exception($"Cant load assembly from {path}.");
+            return asembly;
+        }
+
+    }
+}
--- CodeGeneration.CSharp/CodeGenerationTask.cs
+++ CodeGeneration.CSharp/CodeGenerationTask.cs
@@ -1,44 +1,13 @@
-﻿using Microsoft.Build.Framework;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.MSBuild;
-using System;
-using System.Resources;
-using static CodeGen.CSharp.Context.DocumentEdit.CSharpContextDocumentEditor;
+﻿using static CodeGen.CSharp.Context.DocumentEdit.CSharpContextDocumentEditor;
 
 namespace CodeGeneration.CSharp
 {
-    public abstract class CodeGenerationTask : Microsoft.Build.Utilities.Task
+    public abstract class CodeGenerationTransformer 
     {
-        [Output]
-        public string Compiles { get; set; }
-        public string ProjectPath { get; set; }
-        protected DocumentEditingCodeGenerationEngine Engine { get; set; }
+        public string[] Compiles { get; set; }
+        public DocumentEditingCodeGenerationEngine Engine { get; set; }
 
-        public CodeGenerationTask():base()
-        {
-            var workspace = MSBuildWorkspace.Create();
-            workspace.WorkspaceFailed += (sender, args) =>
-                                            workspace.Diagnostics.Add(args.Diagnostic);
-
-            var Project = workspace.OpenProjectAsync(ProjectPath).Result;
-            Engine = new DocumentEditingCodeGenerationEngine(Project);
-        }
-
-        public override bool Execute()
-        {
-            try
-            {
-                DoWork();
-            }
-            catch (Exception e)
-            {
-                Log.LogErrorFromException(e);
-                return false;
-            }
-            return true;
-        }
-
-        public abstract void DoWork();
+        public abstract void Transform();
 
     }
 }
