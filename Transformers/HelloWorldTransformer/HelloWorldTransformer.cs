08d098f416960cefa8ecc3a8eb56a8d1471bc7f6
Jose10go <Jose10go2017@gmail.com>
2020-02-16 03:51:55 -0500

    implementing hello world transformer

---
+++ Transformers/HelloWorldTransformer/HelloWorldTransformer.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace HelloWorldTransformer
+{
+    public class HelloWorldTransformer : CodeGeneration.CSharp.CodeGenerationTransformer
+    {
+        public override void Transform()
+        {
+            File.WriteAllText("Hello World.txt", "Hello World!");
+        }
+    }
+}
