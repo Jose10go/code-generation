a7d08f0f76c91f6ad8898d091616bcc8100de85a
Jose10go <Jose10go2017@gmail.com>
2020-03-01 13:54:19 -0500

    IWithName Command Modifier

---
+++ CodeGeneration.CSharp/Context/Abstract/Modifiers/IWithName.cs
@@ -0,0 +1,23 @@
+﻿using CodeGen.Attributes;
+using CodeGen.Context;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext<TProcessEntity> : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax, ISymbol, TProcessEntity>
+    {
+        [CommandModifier]
+        public interface IWithName<TCommandBuilder>
+            where TCommandBuilder:Core.ICommand
+        {
+            string Name { get; set; }
+            TCommandBuilder WithName(string Name) 
+            {
+                this.Name = Name;
+                return (TCommandBuilder)this;
+            } 
+        }
+    }
+}
