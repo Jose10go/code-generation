8de06bec626245fd104555d52d0da1a1a2fe5703
Jose10go <Jose10go2017@gmail.com>
2020-03-01 13:55:41 -0500

    helloWorld Transformer Ready

--- Transformers/HelloWorldTransformer/HelloWorldTransformer.cs
+++ Transformers/HelloWorldTransformer/HelloWorldTransformer.cs
@@ -1,10 +1,24 @@
-﻿
+﻿using CodeGen.CSharp.Context.DocumentEdit;
+using System;
+
 namespace HelloWorldTransformer
 {
     public class HelloWorldTransformer : CodeGeneration.CSharp.CodeGenerationTransformer
     {
         public override void Transform()
         {
+            Engine.SelectNew("Program.cs")
+                  .Execute<CSharpContextDocumentEditor.ICreateNamespace>()
+                    .WithName("HelloWorld")
+                    .Execute<CSharpContextDocumentEditor.ICreateClass>()
+                      .WithName("Program")
+                      .MakeStatic()
+                      .Execute<CSharpContextDocumentEditor.ICreateMethod>()
+                        .WithName("Main")
+                        .MakeStatic()
+                        .WithBody((dynamic @this) => { Console.WriteLine("Hello World!!!"); });
+            
+            Engine.ApplyChanges();
         }
     }
 }
