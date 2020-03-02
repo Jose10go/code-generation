acac7e8fcf372dbb4c5f8c4253eeae3cfd7243f3
Jose10go <Jose10go2017@gmail.com>
2020-03-02 02:51:40 -0500

    Using Project.GetChanges to track changes

--- CodeGeneration.CSharp.Precompilation/Program.cs
+++ CodeGeneration.CSharp.Precompilation/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis.MSBuild;
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using static CodeGen.CSharp.Context.DocumentEdit.CSharpContextDocumentEditor;
@@ -10,6 +12,7 @@ namespace CodeGeneration.CSharp.Precompilation
 {
     static class Program
     {
+        [SuppressMessage("Major Code Smell", "S1144:Unused private types or members should be removed", Justification = "System.CommandLine.DragonFruits")]
         static void Main(string project,string[] compiles,string[] transformers)
         {
             MSBuildLocator.RegisterDefaults();
@@ -27,12 +30,23 @@ namespace CodeGeneration.CSharp.Precompilation
             foreach (var item in transformers)
             {
                 var assembly=LoadAssembly(item);
-                var transformer = LoadTransformer(assembly,Engine,compiles);
+                var transformer = LoadTransformer(assembly,Engine);
                 transformer.Transform();
             }
+
+            var changes = Engine.CurrentProject.GetChanges(Project);
+            foreach (var docId in changes.GetAddedDocuments().Union(changes.GetChangedDocuments(true)))
+            {
+                var doc = Engine.CurrentProject.GetDocument(docId);
+                var text = doc.GetTextAsync().Result;//TODO: make async
+                Directory.CreateDirectory(Path.GetDirectoryName(doc.FilePath));
+                File.WriteAllText(doc.FilePath, text.ToString());
+                Console.WriteLine(Path.GetRelativePath(Path.GetDirectoryName(Engine.CurrentProject.FilePath), doc.FilePath));
+            }
+
         }
 
-        private static CodeGenerationTransformer LoadTransformer(Assembly assembly,DocumentEditingCodeGenerationEngine Engine,string[] compiles)
+        private static CodeGenerationTransformer LoadTransformer(Assembly assembly,DocumentEditingCodeGenerationEngine Engine)
         {
             var type=assembly.GetTypes().First(x => x.IsSubclassOf(typeof(CodeGenerationTransformer)));//TODO: Why only one transformer by assembly
             if (type is null) 
@@ -42,7 +56,6 @@ namespace CodeGeneration.CSharp.Precompilation
                 throw new Exception($"Cant get instance of {type.Name}.");
 
             CodeGenerationTransformer trans = transformer as CodeGenerationTransformer;
-            trans.Compiles = compiles;            
             trans.Engine = Engine;
             return transformer as CodeGenerationTransformer;
         }
