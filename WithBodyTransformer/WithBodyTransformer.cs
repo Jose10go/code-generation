7cff5475b02be0ba962e962c2d8c732e5dc6a8dd
Jose10go <Jose10go2017@gmail.com>
2020-03-02 02:49:14 -0500

    adding WithBodyTransformer Project

---
+++ WithBodyTransformer/WithBodyTransformer.cs
@@ -0,0 +1,31 @@
+﻿using CodeGen.CSharp.Context.DocumentEdit;
+using CodeGeneration.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace WithBodyTransformer
+{
+    public class WithBodyTransformer : CodeGenerationTransformer
+    {
+        public override void Transform()
+        {
+            Engine.Select<InvocationExpressionSyntax>()
+                  .Where((symbol, node) =>
+                  {
+                      var s = (IMethodSymbol)symbol;
+                      if (s is null)
+                          return false;
+                      return s.Name == "WithBody" && symbol.ContainingType.Name == "IWithBody";
+                  })
+                  .Execute<CSharpContextDocumentEditor.IReplaceInvocation>()
+                      .WithNewArgument(0, (x) =>
+                      {
+                          var lambda = x.ArgumentList.Arguments[0].Expression as ParenthesizedLambdaExpressionSyntax;
+                          var bodycode = SyntaxFactory.Literal(lambda.Body.ToString().Replace("@this", "this"));
+                          return SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, bodycode));
+                      });
+            Engine.ApplyChanges();
+        }
+    }
+}
