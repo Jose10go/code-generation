6b326e90f3581f44d03572e695a99c9165878bdd
Jose10go <Jose10go2017@gmail.com>
2020-04-21 00:00:27 -0400

    With Attribute Generic overloads

--- src/CSharp/CodeGeneration.CSharp/Modifiers/IWithAttribute.cs
+++ src/CSharp/CodeGeneration.CSharp/Modifiers/IWithAttribute.cs
@@ -3,6 +3,7 @@ using CodeGen.Context;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 namespace CodeGen.CSharp.Context
@@ -24,6 +25,56 @@ namespace CodeGen.CSharp.Context
             TCommandBuilder WithAttributes(params string[] Attributes)
             {
                 return this.WithAttributes(Attributes.AsEnumerable());
+            }
+
+            TCommandBuilder WithAttributes<T>()
+                where T:Attribute
+            {
+                return this.WithAttributes(typeof(T).Name);
+            }
+
+            TCommandBuilder WithAttributes<T1,T2>()
+                where T1 : Attribute
+                where T2 : Attribute
+            {
+                return this.WithAttributes(typeof(T1).Name,
+                                           typeof(T2).Name);
+            }
+
+            TCommandBuilder WithAttributes<T1, T2, T3>()
+                where T1 : Attribute
+                where T2 : Attribute
+                where T3 : Attribute
+            {
+                return this.WithAttributes(typeof(T1).Name, 
+                                           typeof(T2).Name,
+                                           typeof(T3).Name);
+            }
+
+            TCommandBuilder WithAttributes<T1, T2, T3, T4>()
+                where T1 : Attribute
+                where T2 : Attribute
+                where T3 : Attribute
+                where T4 : Attribute
+            {
+                return this.WithAttributes(typeof(T1).Name,
+                                           typeof(T2).Name,
+                                           typeof(T3).Name,
+                                           typeof(T4).Name);
+            }
+
+            TCommandBuilder WithAttributes<T1, T2, T3, T4, T5>()
+               where T1 : Attribute
+               where T2 : Attribute
+               where T3 : Attribute
+               where T4 : Attribute
+               where T5 : Attribute
+            {
+                return this.WithAttributes(typeof(T1).Name,
+                                           typeof(T2).Name,
+                                           typeof(T3).Name,
+                                           typeof(T4).Name,
+                                           typeof(T5).Name);
             }
         }
     }
