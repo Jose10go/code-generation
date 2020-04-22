ebe572613b841bd2dcfea48cdbd6028bed44b17b
Jose10go <Jose10go2017@gmail.com>
2020-04-21 23:36:22 -0400

    ModifyMethodCommand Ready

---
+++ src/CSharp/CodeGeneration.CSharp/Command/ModifyMethodCommand.cs
@@ -0,0 +1,38 @@
+﻿using CodeGen.Context;
+using CodeGen.Core.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax,ISymbol>
+{
+        public interface IModifyMethod : ICommand<MethodDeclarationSyntax,MethodDeclarationSyntax>,
+                                        IGet<IModifyMethod,MethodDeclarationSyntax>,
+                                        IWithName<IModifyMethod>,
+                                        IWithAttribute<IModifyMethod>,
+                                        IWithBody<IModifyMethod>,
+                                        IWithAccessModifier<IModifyMethod>,
+                                        IAbstract<IModifyMethod>,
+                                        IStatic<IModifyMethod>,
+                                        IPartial<IModifyMethod>
+        {
+        }
+
+        [Command]
+        public class ModifyMethodCommand : IModifyMethod
+        {
+            public string Name { get; set; }
+            public SyntaxList<AttributeListSyntax> Attributes{ get; set; }
+            public BlockSyntax Body { get; set; }
+            public ISingleTarget<MethodDeclarationSyntax> SingleTarget { get; set ; }
+            public SyntaxToken Modifiers { get ; set ; }
+            public SyntaxToken Abstract { get ; set ; }
+            public SyntaxToken Static { get ; set ; }
+            public SyntaxToken Partial { get ; set ; }
+        }
+        
+    }
+}
+ 
---
+++ src/CSharp/CodeGeneration.CSharp/CommandHandlers/ModifyMethodHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using CodeGen.Attributes;
+using Microsoft.CodeAnalysis;
+using System;
+using CodeGen.Context;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax, ISymbol>
+    {
+        [CommandHandler]
+        public class ModifyMethodCommandHandler : CommandHandler<IModifyMethod,MethodDeclarationSyntax>
+        {
+            public ModifyMethodCommandHandler(IModifyMethod command) :base(command)
+            {
+            }
+
+            protected override MethodDeclarationSyntax ProccessNode(MethodDeclarationSyntax node,DocumentEditor documentEditor,Guid id)
+            {
+                var modifiers = new SyntaxTokenList();
+                if (Command.Modifiers != default)
+                    modifiers = modifiers.Add(Command.Modifiers);
+                if (Command.Abstract != default)
+                    modifiers = modifiers.Add(Command.Abstract);
+                if (Command.Static != default)
+                    modifiers = modifiers.Add(Command.Static);
+                if (Command.Partial != default)
+                    modifiers = modifiers.Add(Command.Partial);
+                var methodNode = node.WithIdentifier(SyntaxFactory.ParseToken(Command.Name))
+                                               .WithAttributeLists(Command.Attributes)
+                                               .WithBody(Command.Body)
+                                               .WithModifiers(modifiers)
+                                               .WithAdditionalAnnotations(new SyntaxAnnotation($"{id}"));
+
+                documentEditor.ReplaceNode(node,methodNode);
+                return methodNode;
+            }
+        }
+    }
+}
