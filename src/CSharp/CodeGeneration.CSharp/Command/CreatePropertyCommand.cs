b8a0b9d445c669dc075ff1ccdbca1193d7859461
Jose10go <Jose10go2017@gmail.com>
2020-04-22 13:40:19 -0400

    CreatePropertyCommand

---
+++ src/CSharp/CodeGeneration.CSharp/Command/CreatePropertyCommand.cs
@@ -0,0 +1,37 @@
+﻿using CodeGen.Context;
+using CodeGen.Core.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax,ISymbol>
+    {
+        public interface ICreateProperty : ICommand<ClassDeclarationSyntax,PropertyDeclarationSyntax>,
+                                         IWithName<ICreateProperty>,
+                                         IWithAttribute<ICreateProperty>,
+                                         IWithAccessModifier<ICreateProperty>,
+                                         IAbstract<ICreateProperty>,
+                                         IStatic<ICreateProperty>
+        {
+        }
+
+        [Command]
+        public class CreatePropertyCommand : ICreateProperty
+        {
+            public CreatePropertyCommand():base()
+            {
+            }
+
+            public string Name { get; set; }
+            public SyntaxList<AttributeListSyntax> Attributes{ get; set; }
+            public ISingleTarget<ClassDeclarationSyntax> SingleTarget { get; set; }
+            public SyntaxToken Modifiers { get; set; }
+            public SyntaxToken Abstract { get; set; }
+            public SyntaxToken Static { get; set; }
+            public BlockSyntax Body { get ; set ; }
+        }
+
+    }
+}
---
+++ src/CSharp/CodeGeneration.CSharp/CommandHandlers/CreatePropertyHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using CodeGen.Attributes;
+using Microsoft.CodeAnalysis;
+using System;
+using CodeGen.Context;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext:CodeGenContext<Project,CSharpSyntaxNode,CompilationUnitSyntax,ISymbol>
+    {
+        [CommandHandler]
+        public class CreatePropertyCommandHandler : CommandHandler<ICreateProperty,ClassDeclarationSyntax,PropertyDeclarationSyntax>
+        {
+            public CreatePropertyCommandHandler(ICreateProperty command) :base(command)
+            {
+            }
+
+            protected override PropertyDeclarationSyntax ProccessNode(ClassDeclarationSyntax node, DocumentEditor documentEditor,Guid id)
+            {
+                var modifiers = new SyntaxTokenList();
+                if (Command.Modifiers != default)
+                    modifiers = modifiers.Add(Command.Modifiers);
+                if (Command.Abstract != default)
+                    modifiers = modifiers.Add(Command.Abstract);
+                if (Command.Static != default)
+                    modifiers = modifiers.Add(Command.Static);
+                var method = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("object"), Command.Name)
+                                          .WithAttributeLists(Command.Attributes)
+                                          .WithModifiers(modifiers)
+                                          .WithAdditionalAnnotations(new SyntaxAnnotation($"{id}"));
+                documentEditor.InsertMembers(node,0,new[]{method});
+                return method;
+            }
+        }
+    }
+}
