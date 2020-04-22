cb27198da3488edc2cede67c81d047f839345e63
Jose10go <Jose10go2017@gmail.com>
2020-04-22 13:47:39 -0400

    ModifyProperty Command

---
+++ src/CSharp/CodeGeneration.CSharp/Command/ModifyPropertyCommand.cs
@@ -0,0 +1,34 @@
+﻿using CodeGen.Context;
+using CodeGen.Core.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax,ISymbol>
+{
+        public interface IModifyProperty : ICommand<PropertyDeclarationSyntax,PropertyDeclarationSyntax>,
+                                        IGet<IModifyProperty,PropertyDeclarationSyntax>,
+                                        IWithName<IModifyProperty>,
+                                        IWithAttribute<IModifyProperty>,
+                                        IWithAccessModifier<IModifyProperty>,
+                                        IAbstract<IModifyProperty>,
+                                        IStatic<IModifyProperty>
+        {
+        }
+
+        [Command]
+        public class ModifyPropertyCommand : IModifyProperty
+        {
+            public string Name { get; set; }
+            public SyntaxList<AttributeListSyntax> Attributes{ get; set; }
+            public ISingleTarget<PropertyDeclarationSyntax> SingleTarget { get; set ; }
+            public SyntaxToken Modifiers { get ; set ; }
+            public SyntaxToken Abstract { get ; set ; }
+            public SyntaxToken Static { get ; set ; }
+        }
+        
+    }
+}
+ 
---
+++ src/CSharp/CodeGeneration.CSharp/CommandHandlers/ModifyPropertyHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using CodeGen.Attributes;
+using Microsoft.CodeAnalysis;
+using System;
+using CodeGen.Context;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax, ISymbol>
+    {
+        [CommandHandler]
+        public class ModifyPropertyCommandHandler : CommandHandler<IModifyProperty,PropertyDeclarationSyntax>
+        {
+            public ModifyPropertyCommandHandler(IModifyProperty command) :base(command)
+            {
+            }
+
+            protected override PropertyDeclarationSyntax ProccessNode(PropertyDeclarationSyntax node,DocumentEditor documentEditor,Guid id)
+            {
+                var modifiers = new SyntaxTokenList();
+                if (Command.Modifiers != default)
+                    modifiers = modifiers.Add(Command.Modifiers);
+                if (Command.Abstract != default)
+                    modifiers = modifiers.Add(Command.Abstract);
+                if (Command.Static != default)
+                    modifiers = modifiers.Add(Command.Static);
+                var methodNode = node.WithIdentifier(SyntaxFactory.ParseToken(Command.Name))
+                                               .WithAttributeLists(Command.Attributes)
+                                               .WithAccessorList(SyntaxFactory.AccessorList())
+                                               .WithModifiers(modifiers)
+                                               .WithAdditionalAnnotations(new SyntaxAnnotation($"{id}"));
+
+                documentEditor.ReplaceNode(node,methodNode);
+                return methodNode;
+            }
+        }
+    }
+}
