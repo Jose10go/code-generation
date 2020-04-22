f56a50138a4d968a8bb6e449ec8a2bf258d0e937
Jose10go <Jose10go2017@gmail.com>
2020-04-21 21:54:00 -0400

    Create Interface Command

---
+++ src/CSharp/CodeGeneration.CSharp/Command/CreateInterfaceCommand.cs
@@ -0,0 +1,37 @@
+﻿using CodeGen.Context;
+using CodeGen.Core.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax,ISymbol>
+    {
+        public interface ICreateInterface: ICommand<NamespaceDeclarationSyntax, InterfaceDeclarationSyntax>,
+                                       IGet<ICreateInterface,NamespaceDeclarationSyntax>,
+                                       IWithName<ICreateInterface>,
+                                       IWithAttribute<ICreateInterface>,
+                                       IWithAccessModifier<ICreateInterface>,
+                                       IPartial<ICreateInterface>,
+                                       IImplements<ICreateInterface>
+        {
+        }
+
+        [Command]
+        public class CreateInterfaceCommand : ICreateInterface
+        {
+            public CreateInterfaceCommand():base()
+            {
+            }
+
+            public string Name { get; set; }
+            public SyntaxList<AttributeListSyntax> Attributes{ get; set; }
+            public ISingleTarget<NamespaceDeclarationSyntax> SingleTarget { get; set; }
+            public SyntaxToken Modifiers { get; set; }
+            public SyntaxToken Partial { get; set; }
+            public string[] ImplementedInterfaces { get ; set ; }
+        }
+
+    }
+}
---
+++ src/CSharp/CodeGeneration.CSharp/CommandHandlers/CreateInterfaceHandler.cs
@@ -0,0 +1,45 @@
+﻿using CodeGen.Attributes;
+using CodeGen.Context;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using System;
+using System.Linq;
+
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode, CompilationUnitSyntax, ISymbol>
+    {
+        [CommandHandler]
+        public class CreateInterfaceCommandHandler : CommandHandler<ICreateInterface, NamespaceDeclarationSyntax, InterfaceDeclarationSyntax>
+        {
+            public CreateInterfaceCommandHandler(ICreateInterface command) : base(command)
+            {
+            }
+
+            protected override InterfaceDeclarationSyntax ProccessNode(NamespaceDeclarationSyntax node, DocumentEditor documentEditor,Guid id)
+            {
+                var modifiers = new SyntaxTokenList();
+                if (Command.Modifiers != default)
+                    modifiers = modifiers.Add(Command.Modifiers);
+                if (Command.Partial != default)
+                    modifiers = modifiers.Add(Command.Partial);
+
+                var separatedBaseTypes = new SeparatedSyntaxList<BaseTypeSyntax>();
+                if (Command.ImplementedInterfaces != null)
+                    separatedBaseTypes.AddRange(Command.ImplementedInterfaces.Select(name => SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName(name))));
+
+                var classNode = SyntaxFactory.InterfaceDeclaration(Command.Name)
+                                             .WithAttributeLists(Command.Attributes)
+                                             .WithModifiers(modifiers)
+                                             .WithBaseList(SyntaxFactory.BaseList().WithTypes(separatedBaseTypes))
+                                             .WithAdditionalAnnotations(new SyntaxAnnotation($"{id}"));
+
+                documentEditor.InsertMembers(node, 0, new[] { classNode });
+                return classNode;
+            }
+
+        }
+    }
+}
