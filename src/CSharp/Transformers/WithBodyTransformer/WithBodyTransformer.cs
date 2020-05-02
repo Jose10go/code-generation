d2f8ce900a48635793f02281fb6ef5f7fa1e397c
Jose10go <Jose10go2017@gmail.com>
2020-05-02 17:04:59 -0400

    With Body Transformer Upgrade

--- src/CSharp/Transformers/WithBodyTransformer/WithBodyTransformer.cs
+++ src/CSharp/Transformers/WithBodyTransformer/WithBodyTransformer.cs
@@ -2,8 +2,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System;
-using System.Linq;
 using static CodeGen.CSharp.Context.CSharpContext;
 
 namespace WithBodyTransformer
@@ -12,17 +10,33 @@ namespace WithBodyTransformer
     {
         public override void Transform()
         {
-            Engine.Select<ParenthesizedLambdaExpressionSyntax, ArgumentSyntax, InvocationExpressionSyntax>()
-                  .Where(single =>
-                  {
-                      var s = (IMethodSymbol)single.SemanticSymbol;
-                      if (s is null)
-                          return false;
-                      return s.Name == "WithBody" && s.ContainingType.Name == "IWithBody" && !s.Parameters.Any(x => x.Type.Name == "string");
-                  })
-                  .Using(target => target.Node.Body.ToString().Replace("@this", "this"), out var bodyKey)
-                  .Execute((IReplaceExpression<ParenthesizedLambdaExpressionSyntax> cmd) => cmd.Get(bodyKey, out var stringBody)
-                                                                                               .With(SyntaxFactory.ParseExpression($"\"{stringBody}\"")));
+              Engine.Select<ParenthesizedLambdaExpressionSyntax,InvocationExpressionSyntax>()
+                    .Where(x =>x.Parent.SemanticSymbol is IMethodSymbol)
+                    .Where(x=>x.Parent.SemanticSymbol.Name is "WithBody" || 
+                              x.Parent.SemanticSymbol.Name is "WithGet" || 
+                              x.Parent.SemanticSymbol.Name is "WithSet")
+                    .Where(x=>x.Parent.SemanticSymbol.ContainingType.Name is "IWithBody" ||
+                              x.Parent.SemanticSymbol.ContainingType.Name is "IWithGetSet")
+                    .Where(x=>x.Parent.Node.ArgumentList.Arguments.First().Expression==x.Node)
+                        .Using(target => GetBody(target.Node), out var bodyKey)
+                        .Execute((IReplaceExpression<ParenthesizedLambdaExpressionSyntax> cmd) => cmd.Get(bodyKey,out var stringBody)
+                                                                                                     .With(stringBody));
+        }
+
+        private ExpressionSyntax GetBody(ParenthesizedLambdaExpressionSyntax node)
+        {
+            return SyntaxFactory.ParseExpression($"SyntaxFactory.Parse(\"{node}\") as ParenthesizedLambdaExpressionSyntax");
+            
+                //SyntaxFactory.BinaryExpression(SyntaxKind.AsExpression,
+            //           SyntaxFactory.InvocationExpression(
+            //           SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
+            //               SyntaxFactory.IdentifierName(nameof(SyntaxFactory)),
+            //               SyntaxFactory.IdentifierName(nameof(SyntaxFactory.ParseExpression))),
+            //           SyntaxFactory.ArgumentList().AddArguments(
+            //               SyntaxFactory.Argument(
+            //                   SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression,
+            //                       SyntaxFactory.Literal(node.ToString()))))),
+            //           SyntaxFactory.IdentifierName(nameof(ParenthesizedLambdaExpressionSyntax)));
         }
     }
 }
