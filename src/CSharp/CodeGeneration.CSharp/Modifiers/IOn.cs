2e4e75dfbbf316c555c250193eeff89eb099f386
Jose10go <Jose10go2017@gmail.com>
2020-05-07 21:09:58 -0400

    IOn modifier for clone .. its really unndone because need to check for clone on different documents...

---
+++ src/CSharp/CodeGeneration.CSharp/Modifiers/IOn.cs
@@ -0,0 +1,29 @@
+﻿using CodeGen.Attributes;
+using CodeGen.Context;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+namespace CodeGen.CSharp.Context
+{
+    public abstract partial class CSharpContext : CodeGenContext<Project, CSharpSyntaxNode,CompilationUnitSyntax, ISymbol>
+    {
+        [CommandModifier]
+        public interface IOn<TCommand>
+             where TCommand:Core.ICommand,IOn<TCommand>
+        {
+            CSharpSyntaxNode OnNode { get; set; }
+        }
+        
+        [CommandModifier]
+        public interface IOn<TCommand,TSyntaxNode>:IOn<TCommand>
+            where TCommand:Core.ICommand,IOn<TCommand>
+            where TSyntaxNode:CSharpSyntaxNode
+        {
+            TCommand On(TSyntaxNode node) 
+            {
+                this.OnNode = node;
+                return (TCommand)this;
+            }
+        }
+    }
+}
