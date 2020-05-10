ff09bcda42f0a595048ff1ff264ed72a5c6923dd
Jose10go <Jose10go2017@gmail.com>
2020-05-10 01:00:05 -0400

    Adding Using decorators

---
+++ UsingDecorators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Decorators;
+namespace UsingDecorators
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            F();
+        }
+
+        [Crono]
+        static void F() 
+        {
+            System.Threading.Thread.Sleep(10000);
+            Console.ReadLine();
+        }
+    }
+}
