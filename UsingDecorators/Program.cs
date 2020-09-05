4bf7f32d2b3269259f8ecd25407d4332af2a76ac
Jose10go <Jose10go2017@gmail.com>
2020-09-05 15:56:51 -0400

    Using Decorators Example

--- UsingDecorators/Program.cs
+++ UsingDecorators/Program.cs
@@ -1,19 +1,59 @@
 ﻿using System;
+using System.Data;
 using Decorators;
+
 namespace UsingDecorators
 {
-    static class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            F();
+            Console.WriteLine("Write your name:");
+            GetYourName();
+            Console.WriteLine();
+            Console.WriteLine("Write your Age:");
+            GetYourAge();
+            Console.WriteLine();
+            Console.WriteLine("What fibbonaci you like the most?");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine("For the first Time:");
+            Console.WriteLine("Your fib number is "+RecursiveFibb(n));
+            Console.WriteLine();
+            Console.WriteLine("For the second Time:");
+            Console.WriteLine("Your fib number is " + RecursiveFibb(n));
+            Console.WriteLine("Wow!!!that was fast...");
+            Console.WriteLine();
+            Console.WriteLine("Your Random Even Number is " + RandomEvenNumber());
+            Console.WriteLine();
+            Console.ReadLine();
         }
 
         [Crono]
-        static void F() 
+        static string GetYourName()
         {
-            System.Threading.Thread.Sleep(10000);
-            Console.ReadLine();
+            return Console.ReadLine();
+        }
+
+        [Logg]
+        [Crono]
+        static int GetYourAge()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        [Crono, Memoize]
+        static int RecursiveFibb(int n) 
+            => n < 2 ? 1 : RecursiveFibb(n - 1) + RecursiveFibb(n - 2);
+        
+        [Crono]
+        [TransactionRetry]
+        static int RandomEvenNumber() 
+        {
+            Random r = new Random();
+            var n=r.Next(6);
+            if (n % 2 == 0)
+                return n;
+            throw new InvalidConstraintException("Why Odd... i like even...");
         }
     }
 }
