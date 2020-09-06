4ea7e9c22b316ac0b2445222d8267e5a9e3ea007
Jose10go <Jose10go2017@gmail.com>
2020-09-05 20:38:06 -0400

    Upgrade TestDocumentEditingCodeGenerationEngine

--- test/Tester/TestDocumentEditingCodeGenerationEngine.cs
+++ test/Tester/TestDocumentEditingCodeGenerationEngine.cs
@@ -3,15 +3,26 @@ using System.IO;
 using Buildalyzer;
 using Buildalyzer.Workspaces;
 using static CodeGen.CSharp.Context.CSharpContext;
+using System;
 
 namespace Tests
 {
     public class TestDocumentEditingCodeGenerationEngine : CSharpCodeGenerationEngine
     {
-        public static string projectPath = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "Examples", "CommandTests", "CommandTests.csproj"));
-
-        public TestDocumentEditingCodeGenerationEngine() : base(new AnalyzerManager().GetProject(projectPath).AddToWorkspace(new AdhocWorkspace()), new CSharpAutofacResolver())
+        public TestDocumentEditingCodeGenerationEngine() : base(GetProject(), new CSharpAutofacResolver())
         {
+        }
+
+        private static Project GetProject() {
+            var projectPath= Path.GetFullPath(Path.Combine("..", "..", "..", "..", "Examples", "CommandTests", "CommandTests.csproj"));
+            AnalyzerManager manager = new AnalyzerManager();
+            ProjectAnalyzer analyzer = manager.GetProject(projectPath);
+            AdhocWorkspace workspace = new AdhocWorkspace();
+            analyzer.AddBinaryLogger("binarylogtests.binlog");
+            var project = analyzer.AddToWorkspace(workspace);
+            if (project is null || !project.HasDocuments)
+                throw new Exception("Error loading project, check binarylogtests.binlog file for errors.");
+            return project;
         }
     }
 }
